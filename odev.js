92d60deecd2881808fd17dff01eb4d8b7bc087d9
alpercevizz <alperceviz41@gmail.com>
2021-01-07 20:18:20 +0000

    revised

--- odev.js
+++ odev.js
@@ -17,18 +17,21 @@ window.removePet = (id) => {
 
 window.generateDetailModal = (pet) => {
 
-    return ` <div class="modal fade" id="exampleModal${pet.id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
+    return ` <div class="modal fade" id="exampleModal${pet.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
+          <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
           ${pet.name}
+          <p>${pet.description}</p>
         </div>
+
+        <div class = "modal-footer>
+          <button type="button" class ="btn btn-success" data-bs-dismiss="modal">Close</button>
+
       </div>
     </div>
   </div>`
@@ -42,6 +45,7 @@ window.openPetDetail = (id) => {
       const petModalHTMl =  generateDetailModal(data);
       document.querySelector("body").innerHTML += petModalHTMl;
       $(`#exampleModal${id}`).modal('show');
+      document.querySelector(`.exampleModal${id}`)
     })
 
 
